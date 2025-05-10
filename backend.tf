447130a34cdc622c1106702a870b9d978548cc4f
sharaonabishek <sharaonm@bbinsight.com>
2025-05-10 13:55:51 -0500

    Update tf remote state bucket location

--- backend.tf
+++ backend.tf
@@ -4,7 +4,6 @@ terraform {
     key            = "env/dev/terraform.tfstate"
     region         = "us-east-2"
     encrypt        = true
-    use_lock_table = true
     dynamodb_table {
                     name = "terraform-locks"
                     }
