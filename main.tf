1a5c0efb336c49bfe6efe7412dc7fc13fb710f8a
sharaonabishek <sharaonm@bbinsight.com>
2025-05-11 01:15:18 -0500

    Update environment condition check in Jenkinsfile

--- main.tf
+++ main.tf
@@ -1,20 +1,20 @@
-module "kms_key" {
-  source              = "./kms"
-  description         = "My app key"
-  alias_name          = "myapp-key"
-  enable_key_rotation = true
-  tags = {
-    Environment = "dev"
-    Owner       = "Kraken"
-  }
-}
-
-module "s3_bucket" {
-  source        = "./s3"
-  bucket_name   = "kraken-data-lake"
-  force_destroy = true
-  team_name = "Financial Analyst"
-  data_classification = "fin_analysis"
-  environment = "Dev"
-  kms_key_id = module.kms_key.key_arn
-}
+# module "kms_key" {
+#   source              = "./kms"
+#   description         = "My app key"
+#   alias_name          = "myapp-key"
+#   enable_key_rotation = true
+#   tags = {
+#     Environment = "dev"
+#     Owner       = "Kraken"
+#   }
+# }
+#
+# module "s3_bucket" {
+#   source        = "./s3"
+#   bucket_name   = "kraken-data-lake"
+#   force_destroy = true
+#   team_name = "Financial Analyst"
+#   data_classification = "fin_analysis"
+#   environment = "Dev"
+#   kms_key_id = module.kms_key.key_arn
+# }
