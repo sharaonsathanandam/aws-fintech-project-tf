ebff93f32077b99ce5e9437baf6f524774c45557
sharaonabishek <sharaonm@bbinsight.com>
2025-05-12 03:00:15 -0500

    Create cloud trail

--- cloudtrail/variables.tf
+++ cloudtrail/variables.tf
@@ -1,5 +1,4 @@
-variable "log_bucket_name"   { type = string  default = "company-cloudtrail-logs" }
-variable "data_bucket_name"  { type = string  default = "kraken-data-lake-1" }
-
-variable "kms_key_id"        { type = string    }   # encrypt log files
-variable "log_retention_days"{ type = number  default = 365 }    # S3 lifecycle
+variable "kms_key_id"
+  { type = string    }   # encrypt log files
+variable "log_retention_days"
+  { type = number  default = 365 }    # S3 lifecycle
