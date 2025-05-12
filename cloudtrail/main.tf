9491825b17bcfb4e0d19581e44c9b48dd99e3b45
sharaonabishek <sharaonm@bbinsight.com>
2025-05-12 03:38:19 -0500

    Create cloud trail

--- cloudtrail/main.tf
+++ cloudtrail/main.tf
@@ -31,6 +31,10 @@ resource "aws_cloudtrail" "main" {
   include_global_service_events = true
   is_multi_region_trail         = true
   enable_logging                = true
+  event_selector {
+    read_write_type           = "All"
+    include_management_events = true
+}
 }
 
 data "aws_iam_policy_document" "trail_bucket" {
