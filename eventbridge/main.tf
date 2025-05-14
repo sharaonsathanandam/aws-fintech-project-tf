2b82f5d0d40fab151d10cb5b64ed68b0ba5e55b5
sharaonabishek <sharaonm@bbinsight.com>
2025-05-13 23:33:11 -0500

    Create infrastructure for data quality checks

--- eventbridge/main.tf
+++ eventbridge/main.tf
@@ -1,8 +1,8 @@
 data "aws_caller_identity" "current" {}
 
-# data "aws_iam_role" "eb_to_glue_role" {
-#   name = "eb-to-glue-workflow-role"
-# }
+data "aws_iam_role" "eb_to_glue_role" {
+  name = "eb-to-glue-workflow-role"
+}
 
 #Eventbridge rules
 resource "aws_cloudwatch_event_rule" "on_new_data" {
@@ -21,9 +21,9 @@ resource "aws_cloudwatch_event_rule" "on_new_data" {
   })
 }
 
-# resource "aws_cloudwatch_event_target" "trigger_glue" {
-#   rule      = aws_cloudwatch_event_rule.on_new_data.name
-#   arn      = "arn:aws:glue:us-east-2:${data.aws_caller_identity.current.account_id}:workflow/${var.glue_job_name}"
-#   role_arn  = data.aws_iam_role.eb_to_glue_role.arn
-#   input     = jsonencode({ JobName = var.glue_job_name })
-# }
+resource "aws_cloudwatch_event_target" "trigger_glue" {
+  rule      = aws_cloudwatch_event_rule.on_new_data.name
+  arn      = "arn:aws:glue:us-east-2:${data.aws_caller_identity.current.account_id}:workflow/${var.glue_job_name}"
+  role_arn  = data.aws_iam_role.eb_to_glue_role.arn
+  input     = jsonencode({ JobName = var.glue_job_name })
+}
